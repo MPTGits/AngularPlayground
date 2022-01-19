3b65486776b90a86b56dd6dc8d82369e34c0a844
MPTGits <mplamenovt@abv.bg>
2022-01-20 01:36:49 +0200

    Adding template driven froms exercise

---
+++ FormHandling/TemplateDriven/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') singupForm: NgForm
+  defaultQuestion = 'pet'
+  answer = '';
+  genders = ['male', 'female']
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.singupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.singupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+    }
+    });
+  }
+
+  // onSubmit(f: NgForm){
+  //   console.log(f);
+  // }
+
+  onSubmit(){
+    console.log(this.singupForm);
+    this.singupForm.reset();
+  }
+
+}
